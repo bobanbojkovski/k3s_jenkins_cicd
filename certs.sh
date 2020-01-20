12d9cb91c657bf9a0deffa793572bbdc64fe0f49
bobanbojkovski <50116103+bobanbojkovski@users.noreply.github.com>
2020-01-20 22:10:10 +0100

    Update certs.sh

--- certs.sh
+++ certs.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 vault write -format=json pki/issue/example-dot-com \
-        common_name="jenkins.example.com" > certs.json
+        common_name="jenkins.example.com" > /tmp/certs.json
 
-cat certs.json | jq '.data.certificate' > /tmp/tls.crt
-cat certs.json | jq '.data.private_key' > /tmp/tls.key
+cat /tmp/certs.json | jq '.data.certificate' > /tmp/tls.crt
+cat /tmp/certs.json | jq '.data.private_key' > /tmp/tls.key
 
 sed -i 's/\\n/\n/g' /tmp/tls.crt /tmp/tls.key
 sed -i 's/\"//g' /tmp/tls.crt /tmp/tls.key
